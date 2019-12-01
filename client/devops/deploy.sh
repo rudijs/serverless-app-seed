0235db523bf4447cac7d4695a9b52c1027553229
Rudi Starcevic <ooly.me@gmail.com>
2019-12-01 12:14:13 +0800

    Updates

--- client/devops/deploy.sh
+++ client/devops/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 export AWS_RJS_PROFILE=default
-
 export AWS_RJS_ENVIRONMENT=${1:-dev}
 export AWS_RJS_SUBDOMAIN=${2:-app}
 export AWS_RJS_DOMAINNAME=${3:-rudijs.com}
 export RJS_DOMAIN_NAME="${AWS_RJS_ENVIRONMENT}-${AWS_RJS_SUBDOMAIN}.${AWS_RJS_DOMAINNAME}"
+BUCKET_LOCATION=$(aws --profile $AWS_RJS_PROFILE s3api get-bucket-location --bucket dev-app.rudijs.com | jq -r  .LocationConstraint)
 
 echo "==> Building $RJS_DOMAIN_NAME"
 
@@ -24,3 +24,7 @@ export CLOUDFRONT_DISTRIBUTION_ID=$(aws --profile $AWS_RJS_PROFILE cloudfront li
 echo "==> Cloudfront cache invalidation for ${RJS_DOMAIN_NAME} distribution ID ${CLOUDFRONT_DISTRIBUTION_ID}"
 
 aws --profile $AWS_RJS_PROFILE cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
+
+echo "==> http://${RJS_DOMAIN_NAME}.s3-website-${BUCKET_LOCATION}.amazonaws.com"
+
+echo "==> https://${RJS_DOMAIN_NAME}"
