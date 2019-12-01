6218f9d40189a0531d332c9094dd67f1538fa629
Rudi Starcevic <ooly.me@gmail.com>
2019-12-01 22:22:24 +0800

    Add cypress integration tests

---
+++ client/cypress/integration/301_user-sign-in_spec.js
@@ -0,0 +1,19 @@
+describe("The Sign Page", function() {
+  it("user works", function() {
+    const email = Cypress.env("userEmail");
+
+    const password = Cypress.env("userPassword");
+
+    cy.visit("/");
+
+    cy.get("#root > div > nav > div > div:nth-child(2) > a").click();
+
+    cy.get("input[name=email]").type(email);
+
+    cy.get("input[name=password]").type(`${password}{enter}`);
+
+    cy.url().should("include", "/dashboard");
+
+    cy.contains("Can not Admin Link");
+  });
+});
